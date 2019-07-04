83ccc30fa899717a1782bb42f80a4bb82328c408
LK <burreslk@gmail.com>
2019-07-04 21:48:58 +0800

    p

---
+++ test.js
@@ -0,0 +1,25 @@
+
+
+
+var device;
+
+navigator.usb.requestDevice({ filters: [{ vendorId: 0x0483 }] })
+.then(selectedDevice => {
+   device = selectedDevice;
+   console.log('found WebUSB device');
+   return device.open(); // Begin a session.
+ })
+.then(() => device.selectConfiguration(1)) // Select configuration #1 for the device.
+.then(() => device.claimInterface(1)) // Request exclusive control over interface #2.
+.then(() => device.controlTransferOut({
+    requestType: 'class',
+    recipient: 'interface',
+    request: 0x22,
+    value: 0x01,
+    index: 0x02})) // Ready to receive data
+.then(() => device.transferIn(3, 64)) // Waiting for 64 bytes of data from endpoint #3.
+.then(result => {
+  let decoder = new TextDecoder();
+  console.log('Received: ' + decoder.decode(result.data));
+})
+.catch(error => { console.log(error); });
