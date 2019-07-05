399fa137aed1609407f06e9bb5b123c2fec332f0
LK <burreslk@gmail.com>
2019-07-05 17:01:32 +0800

    A

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ console.log("console-log = 0x0004");
 
 
   document.getElementsByTagName("button")[0].onclick = function (e){
-    navigator.usb.requestDevice({ filters: [{ vendorId: 0x0483, productId: 0x5750 }] })
+    navigator.usb.requestDevice({ filters: [{ vendorId: 0x0483, productId: 0x374B }] })
     .then(selectedDevice => {
        device = selectedDevice;
        console.log('found WebUSB device');
